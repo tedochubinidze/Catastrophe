775efc94fbc4b07c40ce8adc2d10923432fe1ab9
nikavargogibedashvili <ngogi12@freeuni.edu.ge>
2014-06-10 23:26:07 +0400

    Comment
    Update comment class

---
+++ src/webPackage/Comment.java
@@ -0,0 +1,27 @@
+package webPackage;
+
+import java.sql.Timestamp;
+
+public class Comment {
+	
+	//Instance variables, dzzmaci
+	public User user;
+	public String text;
+	public Timestamp timestamp;
+	
+	//Public constructor for this type.
+	public Comment(User user, String text, Timestamp timestamp) {
+		this.user = user;
+		this.text = text;
+		this.timestamp = timestamp;
+	}
+	
+	//My version of toString for comments.
+	@Override
+	public String toString() {
+		String str = "";
+		str += "User: " + user.ID + " text: " + text + " date" + timestamp;
+		return str;
+	}
+	
+}
