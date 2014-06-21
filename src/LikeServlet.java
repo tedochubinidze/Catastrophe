0db571136cd2e0071bc0c88f41525b5e4a74ceef
tedochubinidze <tchub12@freeuni.edu.ge>
2014-06-22 03:03:47 +0400

    Logout, dislike, like
    დავამატე Logout , Dislike, Like სევლეტები

---
+++ src/LikeServlet.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import webPackage.Post;
+import webPackage.User;
+
+/**
+ * Servlet implementation class LikeServlet
+ */
+@WebServlet("/LikeServlet")
+public class LikeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LikeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Post post = (Post)request.getServletContext().getAttribute("currentPost");
+		User user = (User)request.getSession().getAttribute("currentUser");
+		post.likePost(user.getID());
+		RequestDispatcher dp = request.getRequestDispatcher("post.jsp?id=" + post.getID());
+		dp.forward(request, response);
+		
+	}
+
+}
