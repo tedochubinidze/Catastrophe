672f0971d1787c3f2afb07957c11131044df2611
tedochubinidze <tchub12@freeuni.edu.ge>
2014-06-24 19:28:56 +0400

    Panels
    დავამატე ადმინის და იუზერის პანელი

--- src/webPackage/MyDBInfo.java
+++ src/webPackage/MyDBInfo.java
@@ -10,7 +10,7 @@ package webPackage;
 public class MyDBInfo {
 	
 	public static final String MYSQL_USERNAME = "root";
-	public static final String MYSQL_PASSWORD = "blabla";
+	public static final String MYSQL_PASSWORD = "12345tedo";
 	public static final String MYSQL_DATABASE_SERVER = "localhost";
 	public static final String MYSQL_DATABASE_NAME = "test_web";
 	
