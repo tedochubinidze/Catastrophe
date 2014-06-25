5ce8885070123f6ff126821b71797848c03dd67c
nikavargogibedashvili <ngogi12@freeuni.edu.ge>
2014-06-25 06:12:02 +0400

    DeleteOrderServlet

---
+++ src/DeleteOrderServlet.java
@@ -0,0 +1,46 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import managers.ProductManager;
+
+/**
+ * Servlet implementation class DeleteOrderServlet
+ */
+@WebServlet("/DeleteOrderServlet")
+public class DeleteOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteOrderServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductManager manager = (ProductManager)request.getServletContext().getAttribute("productManager");
+		manager.deleteOrder(Integer.parseInt(request.getParameter("orderID")));
+		RequestDispatcher rd = request.getRequestDispatcher("orders.jsp");
+		rd.forward(request, response);
+	}
+
+}
